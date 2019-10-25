13b758ae74da66dec772d4f2fe6fa10d9e036d57
Josh Rule <joshua.s.rule@gmail.com>
2019-10-25 11:52:22 -0400

    trs::rewrite: refactor replace_term_with_var

--- src/trs/rewrite/replace_term_with_var.rs
+++ src/trs/rewrite/replace_term_with_var.rs
@@ -8,33 +8,26 @@ use super::{SampleError, TRS};
 impl TRS {
     /// Replace a subterm of the rule with a variable.
     pub fn replace_term_with_var<R: Rng>(&self, rng: &mut R) -> Result<Vec<TRS>, SampleError> {
-        let old_trs = self.clone(); // TODO: cloning is a hack!
         let clause = self.choose_clause(rng)?;
         let mut types = HashMap::new();
-        old_trs.lex.infer_rule(&clause, &mut types).drop()?;
+        self.lex.infer_rule(&clause, &mut types).drop()?;
         let new_trss = clause
             .lhs
             .subterms()
             .iter()
             .unique_by(|(t, _)| t)
             .filter_map(|(t, p)| {
-                // need to indicate that p is from the LHS
-                let mut real_p = p.clone();
-                real_p.insert(0, 0);
+                let mut real_p = vec![0]; // indicates that p is from the LHS
+                real_p.extend_from_slice(p);
                 let mut trs = self.clone();
                 let term_type = &types[&real_p];
                 let new_var = trs.lex.invent_variable(term_type);
                 let new_term = Term::Variable(new_var);
                 let new_lhs = clause.lhs.replace_all(t, &new_term);
                 let new_rhs = clause.rhs().unwrap().replace_all(t, &new_term);
-                Rule::new(new_lhs, vec![new_rhs]).and_then(|new_clause| {
-                    let okay = trs.replace(&clause, new_clause).is_ok();
-                    if okay {
-                        Some(trs)
-                    } else {
-                        None
-                    }
-                })
+                let new_clause = Rule::new(new_lhs, vec![new_rhs])?;
+                trs.replace(&clause, new_clause).ok()?;
+                Some(trs)
             })
             .collect_vec();
         if new_trss.is_empty() {
